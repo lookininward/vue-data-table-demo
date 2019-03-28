f104bfc26fe05f161a7acfe027e11dc0511a74b5
MichaelX <vinoth.michaelxavier@gmail.com>
2019-03-28 09:51:36 -0400

    [TEST] Verify Data Table component renders

---
+++ src/components/DataTable.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    data-test-component="DataTable"
+    class="dataTable"
+  >
+    {{ header }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataTable',
+  props: {
+    header: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
