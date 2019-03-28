0c3ef278786252656068142985389c9be71d8a79
Michael Xavier <vinoth.michaelxavier@gmail.com>
2019-03-28 19:06:01 -0400

    Load data from local json db server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,39 +17,24 @@ export default {
     DataTable
   },
 
-  // data to be fetched in the Home route and passed to DataTable
   data() {
-     let items = [];
-     for (let i = 0; i < 100; i++) {
-      items.push({
-        id: '3471DA17-401F-9633-BF81-4CADA6FD5C79',
-        name: 'Kyra Lester',
-        description: 'Curabitur dictum. Phasellus in',
-        date: '2017-07-23T04:24:49-07:00',
-        amount: '345.54',
-        secret: 'Monkey!!!'
-      });
-       items.push({
-        id: '9F5C9912-936A-FB85-1EDB-9DA87BE7FF1E',
-        name: 'Buckminster Alvarado',
-        description: 'dui, in sodales elit erat vitae risus. Duis a mi',
-        date: '2018-11-08T05:44:15-08:00',
-        amount: '677.08',
-        secret: 'Monkey!!!'
-      });
-       items.push({
-        id: 'B743AC82-3613-13A2-2E42-E0C1F5CBF8A6',
-        name: 'Athena Smith',
-        description: 'massa lobortis ultrices. Vivamus rhoncus.',
-        date: '2018-11-11T06:19:57-08:00',
-        amount: '73.67',
-        secret: 'Monkey!!!'
-      });
+    return {
+      items: null
+    }
+  },
+
+  created() {
+    this.fetchData()
+  },
+
+  methods: {
+
+    fetchData() {
+      fetch('http://localhost:3000/items')
+      .then(res => { return res.text() })
+      .then(data => { this.items = JSON.parse(data) })
     }
 
-    return {
-      items
-    }
   }
 
 }
