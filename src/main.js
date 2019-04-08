14e971989596888c7cbf1c60e8d19c3a78e69eb5
Michael Xavier <vinoth.michaelxavier@gmail.com>
2019-04-08 10:14:24 -0400

    add graphql endpoint config

--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Amplify, * as AmplifyModules from 'aws-amplify';
+import { API } from "aws-amplify";
 import { AmplifyPlugin } from 'aws-amplify-vue';
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
 Vue.use(AmplifyPlugin, AmplifyModules);
 Vue.config.productionTip = false
+
+Amplify.configure({
+  API: {
+    graphql_endpoint: 'https://rdtiduxvafeevmr2u5qc5u6mue.appsync-api.us-east-2.amazonaws.com/graphql'
+  }
+});
 
 new Vue({
   router,
