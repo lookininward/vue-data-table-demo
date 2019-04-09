f225b2bbfa0af7478fe0ee269b2fe4ea6e8cc0c3
Michael Xavier <vinoth.michaelxavier@gmail.com>
2019-04-09 12:23:25 -0400

    Move ui css into App

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,94 @@
 <!-- Style ------------------------------------------------------------------->
 <style lang="scss">
 
-body {
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-}
+  body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
 
-#app {
-  @include fontStandard();
-  user-select: none;
-}
+  #app {
+    @include fontStandard();
+    user-select: none;
+  }
 
-h3 {
-  margin: 0;
-}
+  h3 {
+    margin: 0;
+  }
 
-input[type='checkbox'] {
-  cursor: pointer;
-}
+  input[type='checkbox'] {
+    cursor: pointer;
+  }
+
+  // Breakout UI components
+  //-- Buttons ----------------------------------
+  .btn {
+    min-width: 20px;
+    background-color: $bg-color--light;
+    border: 1px solid $bdr-color--light;
+    border-radius: 10px;
+    padding: 3px 5px;
+    font-size: $font-sm;
+    cursor: pointer;
+    outline: 0;
+    transition: all .2s;
+    @include hoverState();
+    @include activeState();
+  }
+
+  .btn.btn--confirm {
+    &:hover {
+      background-color: $bg-color--green;
+      color: $txt-color--light;
+    }
+  }
+
+  .btn.btn--pageNumber {
+    margin: 0 2px;
+    padding: 2px 10px;
+
+    &:last-child {
+      margin: 0 0 0 2px;
+    }
+  }
+
+  //-- Inputs -----------------------------------
+  i.input.input--checkbox {
+    font-size: $font-lg;
+    cursor: pointer;
+
+    @media screen and (min-width: $screen-width-sm) {
+      margin: 2px;
+    }
+  }
+
+  //-- Popovers ---------------------------------
+  .popover-theme {
+    @include tippyBaseTheme();
+  }
+
+  .popover {
+    @include fontStandard();
+    text-align: left;
+    background-color: $bg-color--light;
+    color: $txt-color--dark;
+  }
+
+  .popover.popover--standard {
+    @include fontStandard();
+    text-align: left;
+    padding: 10px 15px;
+    background-color: $bg-color--light;
+    color: $txt-color--dark;
+  }
+
+  .popover-options-list {
+    .popover-option {
+      width: 100%;
+      padding: 3px 8px;
+      @include activeState();
+      @include hoverState();
+      cursor: pointer;
+    }
+  }
 </style>
